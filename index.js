ba3e646784e7cde7f4a2d1662528da0ef6a9ce41
miklizak12 <156518822+miklizak12@users.noreply.github.com>
2024-01-13 13:45:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let a1;
-let h1;
-let p1;
+let a;
+let h;
+let p;
 
-document.getElementById("oblicz1").onclick = function(){
-    a1 = document.getElementById("a1").value
-    a1 = Number(a1);
-    h1 = document.getElementById("h1").value
-    h1 = Number(h1);
-    p1 = (a1 * h1) / 2;
-    document.getElementById("wynik1").changeTextContent = p1 + ' cm²';
+document.getElementById("oblicz").onclick = function(){
+    a = document.getElementById("a").value
+    a = Number(a);
+    h = document.getElementById("h").value
+    h = Number(h);
+    p = (a * h) / 2;
+    document.getElementById("wynik").textContent = p + ' cm²';
 }
 
 
